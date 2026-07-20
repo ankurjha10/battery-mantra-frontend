c5def61b3dea5aa13efe0aa4daec98a0b0249349
Ankur Jha <ankurjha521@gmail.com>
2026-07-20 12:34:26 +0530

    fix: apply min-w-0 to main content areas to prevent layout overflow in sidebar containers

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -17,7 +17,7 @@ function AdminLayout() {
             <SidebarTrigger />
             <div className="flex-1" />
           </header>
-          <main className="flex-1 p-4 sm:p-6 lg:p-8">
+          <main className="flex-1 min-w-0 p-4 sm:p-6 lg:p-8">
             <Outlet />
           </main>
         </SidebarInset>
